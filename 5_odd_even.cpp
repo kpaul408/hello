fe4be72a5449b1eb94d98306651ad3cb27f65813
kpaul408 <76876277+kpaul408@users.noreply.github.com>
2021-01-31 09:11:49 +0530

    Add files via upload

---
+++ 5_odd_even.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("Enter the Number: ");
+	scanf("%d",&a);
+	if(a%2==0){
+		printf("This number is even");
+	}else{
+		printf("This number is odd");
+	}
+}
